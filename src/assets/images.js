0ef307cab30eeffcc8f1eabdbc1c65f566578650
RusselJay27 <ponferrada.russel27@gmail.com>
2023-10-09 09:10:41 +0800

    added assets images n videos

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,3 +1,11 @@
 export default images = {
     logo: require('~/assets/images/logo.png'),
-  };
+    burn: require('~/assets/images/burn.png'),
+    sprain: require('~/assets/images/sprain.png'),
+    wounds: require('~/assets/images/wounds.png'),
+    injuredarm: require('~/assets/images/injuredarm.png'),
+    heatexhaustion: require('~/assets/images/heatexhaustion.png'),
+    adult: require('~/assets/images/adult.png'),
+    children: require('~/assets/images/children.png'),
+    infant: require('~/assets/images/infant.png'),
+};
---
+++ src/assets/videos.js
@@ -0,0 +1,7 @@
+export default videos = {
+    burn: require('~/assets/videos/burn.mp4'),
+    sprain: require('~/assets/videos/sprain.mp4'),
+    wounds: require('~/assets/videos/wounds.mp4'),
+    injuredarm: require('~/assets/videos/injuredarm.mp4'),
+    heatexhaustion: require('~/assets/videos/heatexhaustion.mp4'),
+};
