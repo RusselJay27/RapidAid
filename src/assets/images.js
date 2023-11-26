ae106e8dda24c5a116bcea88304c59373f1f2334
RusselJay27 <ponferrada.russel27@gmail.com>
2023-11-27 00:59:52 +0800

    added images in assets

--- src/assets/images.js
+++ src/assets/images.js
@@ -8,4 +8,21 @@ export default images = {
     adult: require('~/assets/images/adult.png'),
     children: require('~/assets/images/children.png'),
     infant: require('~/assets/images/infant.png'),
+    
+    bls: require('~/assets/images/bls.jpg'),
+    disaster_risk_reduction: require('~/assets/images/disaster_risk_reduction.jpg'),
+    hotline: require('~/assets/images/hotline.jpg'),
+    quiz: require('~/assets/images/quiz.jpg'),
+
+    flood: require('~/assets/images/flood.jpg'),
+    storm_surge: require('~/assets/images/storm_surge.jpg'),
+    tropical_cyclone: require('~/assets/images/tropical_cyclone.jpg'),
+    earthquake: require('~/assets/images/earthquake.jpg'),
+    landslide: require('~/assets/images/landslide.jpg'),
+    fire: require('~/assets/images/fire.jpg'),
+    
+    allergic_reaction: require('~/assets/images/allergic_reaction.jpg'),
+    fracture: require('~/assets/images/fracture.jpg'),
+    stroke: require('~/assets/images/stroke.jpg'),
+    eye_injury: require('~/assets/images/eye_injury.jpg'),
 };
